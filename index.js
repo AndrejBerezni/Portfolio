3bf0f8218afe68c1b8744e0b2a4ca5fadf1ad06c
Andrej Berezni <berezniandrej@gmail.com>
2023-02-11 12:49:49 +0000

    finish stack section

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show')
-        }
+            entry.target.classList.add('show');}
+        //  else {
+        //     entry.target.classList.remove('show')
+        // } <--In case you want to make effect repeat
         
     })
 })
-const imgObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('showImg');
-        }
+// const imgObserver = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('showImg');
+//         }
         
-    })
-})
+//     })
+// })
 
 const hiddenElements = document.querySelectorAll('.hidden');
-const hiddenImage = document.querySelectorAll('.hiddenImg')
+// const hiddenImage = document.querySelectorAll('.hiddenImg');
 
 hiddenElements.forEach((el) => observer.observe(el));
-hiddenImage.forEach((el) => imgObserver.observe(el));
+// hiddenImage.forEach((el) => imgObserver.observe(el));
